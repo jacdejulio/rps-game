304765b86d919ac4fa281fdab0f6869edfffab53
jacdejulio <76726688+jacdejulio@users.noreply.github.com>
2023-10-04 08:20:13 +0800

    Updated JS and Index file for DOM manipulation

--- app.js
+++ app.js
@@ -1,36 +1,58 @@
 // ##GET COMPUTER CHOICE##
 function getComputerChoice() {
-    let nums = Math.floor(Math.random() * 4)
-    let compChoice = ''
+    let nums = Math.floor(Math.random() * 4);
+    let compChoice = '';
 
     switch (nums) {
         case 0:
-            nums = Math.floor(Math.random() * 4)
+            nums = Math.floor(Math.random() * 4);
         case 1:
-            compChoice = "rock"
+            compChoice = "rock";
             break;
         case 2:
-            compChoice = "paper"
+            compChoice = "paper";
             break;
         case 3:
-            compChoice = "scissors"
+            compChoice = "scissors";
             break;
     }
     return compChoice
 }
 
 // ##GET PLAYER CHOICE##
-function getPlayerSelection() {
-    const rps = ["rock", "paper", "scissors"]
-    let playerChoice = prompt("Please choose rock, paper or scissors: ")
+const btnRock = document.querySelector(".rock");
+const btnPaper = document.querySelector(".paper");
+const btnScissors = document.querySelector(".scissors");
+const showWin = document.querySelector(".result");
+const score = document.querySelector(".score");
 
-    while (!rps.includes(playerChoice.toLowerCase())) {
-        playerChoice = prompt("Please choose rock, paper or scissors: ")
-    }
+let winPlayer = 0;
+let winComp = 0;
 
-    playerChoice = playerChoice.toLowerCase()
-    return playerChoice
-}
+
+// ##EVENT LISTENER FOR EACH BUTTON
+
+btnRock.addEventListener("click", function () {
+    btnRock.value = "rock";
+    let PlayerSelection = btnRock.value;
+    const roundWinner = playRound(getComputerChoice(), PlayerSelection);
+    updateScore(roundWinner);
+});
+
+btnPaper.addEventListener("click", function () {
+    btnPaper.value = "paper";
+    let PlayerSelection = btnPaper.value;
+    const roundWinner = playRound(getComputerChoice(), PlayerSelection);
+    updateScore(roundWinner);
+});
+
+btnScissors.addEventListener("click", function () {
+    btnScissors.value = "scissors";
+    let PlayerSelection = btnScissors.value;
+    const roundWinner = playRound(getComputerChoice(), PlayerSelection);
+    updateScore(roundWinner);
+});
+
 
 
 // rock beats scissors
@@ -38,18 +60,15 @@ function getPlayerSelection() {
 // scissors beats paper
 
 
-function playRound() {
+function playRound(comp, player) {
 
-    let comp = getComputerChoice()
-    let player = getPlayerSelection()
-    let roundWin = ''
     let result = ''
 
     if ((player === "rock" && comp === "scissors") ||
         (player === "paper" && comp === "rock") ||
         (player === "scissors" && comp === "paper")) {
 
-        console.log(`You win, ${player} beats ${comp}.`)
+        showWin.textContent = `You win, ${player} beats ${comp}.`
         result = "player"
 
 
@@ -57,44 +76,55 @@ function playRound() {
         (comp === "paper" && player === "rock") ||
         (comp === "scissors" && player === "paper")) {
 
-        console.log(`You lose, ${player} loses over ${comp}.`)
+        showWin.textContent = `You lose, ${player} loses over ${comp}.`
         result = "computer"
 
     } else if (comp === player) {
 
-        console.log(`It's a draw. You both chose ${comp}.`)
+        showWin.textContent = `It's a draw. You both chose ${comp}.`
         result = "draw"
     }
 
     return result
+
+
 }
 
-function game() {
-    let champ = ''
-    let winComp = 0
-    let winPlayer = 0
 
 
-
-    for (let i = 0; i < 5; i++) {
-        let roundWinner = playRound()
-        if (roundWinner === "player") {
-            winPlayer++
-        } else if (roundWinner === "computer") {
-            winComp++
-        }
-
-        console.log(`Player: ${winPlayer}               Computer: ${winComp}`)
+function updateScore(roundWinner) {
+    if (roundWinner === "player") {
+        winPlayer++;
+    } else if (roundWinner === "computer") {
+        winComp++;
     }
 
+    score.textContent = `Player: ${winPlayer}               Computer: ${winComp}`;
+
+    if (winPlayer === 5 || winComp === 5) {
+        endGame();
+    }
+
+}
+
+function endGame() {
+    const final = document.createElement("div")
+    score.appendChild(final)
+
+    let champ = '';
 
     if (winPlayer > winComp) {
-        champ = `Overall winner is PLAYER with a score of ${winPlayer} vs ${winComp}!`
+        champ = `Overall winner is PLAYER with a score of ${winPlayer} vs ${winComp}!`;
     } else if (winPlayer < winComp) {
-        champ = `Overall winner is COMPUTER with a score of ${winComp} vs ${winPlayer}!`
+        champ = `Overall winner is COMPUTER with a score of ${winComp} vs ${winPlayer}!`;
     } else {
-        champ = `It's a draw with a score of ${winComp}`
+        champ = `It's a draw with a score of ${winComp}`;
     }
 
-    alert(champ)
+    final.textContent = champ;
+
+    winPlayer = 0;
+    winComp = 0;
+
+
 }
